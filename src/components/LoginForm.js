c5abf57bec977299a42515bf85096f11e35fc0a2
Suelen Arruda <suelenonyrius@gmail.com>
2022-03-11 10:57:45 -0300

    adiciona base css  do Login  e estruturas de input e submit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,12 +4,27 @@ export default class LoginForm extends Component {
   render() {
     return (
       <div>
-        <h1>Trybe Wallet</h1>
+        <h1 className="title">Trybe Wallet</h1>
         <form className="login-form-component">
           <label htmlFor="loginEmail">
-            <input type="text" name="email" id="loginEmail" />
+            <input
+              data-testid="email-input"
+              placeholder="Email"
+              type="email"
+              name="email"
+              id="loginEmail"
+            />
           </label>
-          <input type="submit" value="Enviar" />
+          <label htmlFor="password">
+            <input
+              data-testid="password-input"
+              placeholder="Senha"
+              type="password"
+              name="password"
+              id="password"
+            />
+          </label>
+          <input className="btn-login" type="submit" value="Enviar" />
         </form>
 
       </div>
